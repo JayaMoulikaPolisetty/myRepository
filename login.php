406108e899641951ccda2d692b33f1296ad12797
mouli <mouli>
2016-01-23 20:33:30 +0530

    my todo

---
+++ login.php
@@ -0,0 +1,38 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+	<title>To Do</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+           <link rel="stylesheet" href="todo.css">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <hr>
+</head>
+<body>
+      <div class="jumbotron">
+        <h1>Todo List </h1>
+		
+	<form action="check.php" method="post" class="form-signin">
+        <h2 class="form-signin-heading">Please sign in</h2>
+	
+        <input name="username" type="text" class="input-block-level" placeholder="Username" maxlength="20"><br>
+       	<input name="password" type="password" class="input-block-level" placeholder="Password" maxlength="20"> <br>
+	
+        <input type="submit" class="btn btn-large btn-primary"  value="sign in"></form>
+      
+	 <form action="register1.php" method="post" class="form-register"> 
+	 <input type="submit" class="btn btn-large btn-primary"  value="Register"></form>
+      
+	
+      </div>
+</body>
+</html>
+
+
+
+      
+#myRepository
